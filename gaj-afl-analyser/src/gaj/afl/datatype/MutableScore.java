6cc5f0c80334b231a2fcceeffdef777c8e61b786
uoa1637653 <geoff.jarrad@hotmail.com.au>
2014-04-02 14:15:13 +1030

    Forgot to add this one

---
+++ gaj-afl-analyser/src/gaj/afl/datatype/MutableScore.java
@@ -0,0 +1,20 @@
+package gaj.afl.datatype;
+
+public interface MutableScore extends Score {
+
+	/**
+	 * Adds the given score to the current score.
+	 * 
+	 * @param score - The incremental score.
+	 */
+	public void add(Score score);
+
+	/**
+	 * Adds the given score to the current score.
+	 * 
+	 * @param numGoals - The incremental number of goals.
+	 * @param numBehinds - The incremental number of behinds.
+	 */
+	public void add(int numGoals, int numBehinds);
+
+}
