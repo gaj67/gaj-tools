ce0a13dd9ad79649671594b66de8dd2fc9772486
gaj67 <geoff.jarrad@gmail.com>
2014-05-04 00:06:32 +0930

    Pushed notes into implementation.

--- gaj-data-analysis/src/gaj/analysis/curves/CurveFactory.java
+++ gaj-data-analysis/src/gaj/analysis/curves/CurveFactory.java
@@ -14,19 +14,6 @@ public abstract class CurveFactory {
 	 * Estimates the scalar distance r
 	 * needed to reach the turning point
 	 * at x=x0+r*g0.
-	 * <p/>Consider a curve y=f(x), such that
-	 * y0=f(x0), g0=f'(x0), H0=f''(x0), etc.
-	 * Let x1=x0+r0*g0. Then (Taylor series)
-	 * f(x1) = f(x0)+f'(x0).(x1-x0)+1/2 f''(x0):(x1-x0)^2,
-	 * => y1 = y0+r0*g0.g0+1/2*r0^2*g0.H0.g0.
-	 * Also, 
-	 * f'(x1) = f'(x0)+f''(x0).(x1-x0)
-	 * => g1 = g0+r0*H0.g0 => g0.H0.g0 = g0.(g1-g0)/r0.
-	 * Now, optimum occurs at x=x0+r*g0 with 0 slope, i.e.
-	 *    0 = f'(x) = f'(x0)+f''(x0).(x-x0)
-	 * => 0 = g0+r*H0.g0 
-	 * => r = -g0.g0/g0.H0.g0 
-	 *     = -r0*g0.g0/g0.(g1-g0).
 	 * 
 	 * @param r0 - The initial gradient scaling.
 	 * @param g0 - The slope at x0.
@@ -36,6 +23,21 @@ public abstract class CurveFactory {
 	public static double quadraticOptimum(
 			double r0, DataObject g0, DataObject g1) 
 	{
+		/*
+		 * <p/>Consider a curve y=f(x), such that
+		 * y0=f(x0), g0=f'(x0), H0=f''(x0), etc.
+		 * Let x1=x0+r0*g0. Then (Taylor series)
+		 * f(x1) = f(x0)+f'(x0).(x1-x0)+1/2 f''(x0):(x1-x0)^2,
+		 * => y1 = y0+r0*g0.g0+1/2*r0^2*g0.H0.g0.
+		 * Also, 
+		 * f'(x1) = f'(x0)+f''(x0).(x1-x0)
+		 * => g1 = g0+r0*H0.g0 => g0.H0.g0 = g0.(g1-g0)/r0.
+		 * Now, optimum occurs at x=x0+r*g0 with 0 slope, i.e.
+		 *    0 = f'(x) = f'(x0)+f''(x0).(x-x0)
+		 * => 0 = g0+r*H0.g0 
+		 * => r = -g0.g0/g0.H0.g0 
+		 *     = -r0*g0.g0/g0.(g1-g0).
+		 */
 		double g0g0 = NumericFactory.dot(g0, g0);
 		double g0g1 = NumericFactory.dot(g0, g1);
 		return -r0 * g0g0 / (g0g1 - g0g0);
@@ -49,14 +51,6 @@ public abstract class CurveFactory {
 	 * needed to reach the turning point
 	 * at x=x1+r*g1.
 	 * 
-	 * Compute distance from x0 along g0
-	 * (see quadraticOptimum), then
-	 * correct for distance from x1=x0+r0*g0 along g0,
-	 * then project onto g1.
-	 * => r1 = (r-r0)*g0.g1/g1.g1
-	 *       = [-r0*g0.g0/g0.(g1-g0)-r0]*g0.g1/g1.g1
-	 *       = -r0 * g0.g1*g0.g1 / g0.(g1-g0)*g1.g1.
-     *
 	 * @param r0 - The initial gradient scaling.
 	 * @param g0 - The slope at x0.
 	 * @param g1 - The slope at x1=x0+r0*g0.
@@ -65,6 +59,14 @@ public abstract class CurveFactory {
 	public static double quadraticOptimum2(
 			double r0, DataObject g0, DataObject g1) 
 	{
+		 /* Compute distance from x0 along g0
+		 * (see quadraticOptimum), then
+		 * correct for distance from x1=x0+r0*g0 along g0,
+		 * then project onto g1.
+		 * => r1 = (r-r0)*g0.g1/g1.g1
+		 *       = [-r0*g0.g0/g0.(g1-g0)-r0]*g0.g1/g1.g1
+		 *       = -r0 * g0.g1*g0.g1 / g0.(g1-g0)*g1.g1.
+		 */
 		double g0g0 = NumericFactory.dot(g0, g0);
 		double g0g1 = NumericFactory.dot(g0, g1);
 		double g1g1 = NumericFactory.dot(g1, g1);
