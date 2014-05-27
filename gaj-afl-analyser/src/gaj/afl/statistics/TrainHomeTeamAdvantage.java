78cceb49f99a20e84d3d02d14b9b078383d62724
uoa1637653 <geoff.jarrad@hotmail.com.au>
2014-05-27 15:49:16 +0930

    Revamped previous code.

--- gaj-afl-analyser/src/gaj/afl/statistics/TrainHomeTeamAdvantage.java
+++ gaj-afl-analyser/src/gaj/afl/statistics/TrainHomeTeamAdvantage.java
@@ -1,19 +1,13 @@
 package gaj.afl.statistics;
+import gaj.afl.classifier.LoggedClassifierTrainer;
 import gaj.afl.data.MatchDataFactory;
 import gaj.afl.data.classifier.GoldMatchDataNoDraws;
 import gaj.afl.data.match.Match;
 import gaj.afl.data.match.MatchFetcher;
-import gaj.analysis.classifier.AccuracyScorer;
 import gaj.analysis.classifier.ClassifierFactory;
-import gaj.analysis.classifier.LogProbScorer;
-import gaj.analysis.numeric.NumericFactory;
 import gaj.analysis.vector.VectorFactory;
-import gaj.data.classifier.DataScorer;
 import gaj.data.classifier.GoldData;
 import gaj.data.classifier.GoldDatum;
-import gaj.data.classifier.TrainableClassifier;
-import gaj.data.classifier.TrainingControl;
-import gaj.data.classifier.TrainingSummary;
 import gaj.data.vector.DataVector;
 
 import java.util.Collection;
@@ -37,63 +31,16 @@ public class TrainHomeTeamAdvantage {
 		System.out.printf("#games=%d, home-losses=%d, home-wins=%d, P(home-win)=%5.3f, P(home-loss)=%5.3f%n", n, n-w, w, p, 1-p);
 		System.out.printf("Expected parameter=%f%n", Math.log((1-p) / p));
 		GoldData testingData = getMatchData(manager.getMatchesByYear(2012, 2013));
-		DataScorer[] scorers = new DataScorer[] {
-			new LogProbScorer(trainingData),
-			new AccuracyScorer(trainingData, 1e-3),
-			new LogProbScorer(testingData),
-			new AccuracyScorer(testingData, 1e-3),
-		};
-		train(false, scorers);
-		train(true, scorers);
+		getTrainer(trainingData, testingData).train(false, false);
+		getTrainer(trainingData, testingData).train(true, true);
 	}
 
-	private static void train(boolean useAcceleration, DataScorer[] scorers) {
-		System.out.printf("Using acceleration: %s%n", useAcceleration);
-		int numClasses = scorers[0].numClasses();
-		int numFeatures = scorers[0].numFeatures();
-		TrainableClassifier classifier = ClassifierFactory.newDefaultClassifier(numClasses, numFeatures);
-		TrainingControl control = getControl(useAcceleration);
-		TrainingSummary summary = classifier.getTrainer(scorers).train(control);
-		System.out.printf("#iterations=%d%n", summary.numIterations());
-		printScores("Initial", summary.initalScores());
-		printScores("Final", summary.finalScores());
-		NumericFactory.display("Final classifier parameter=", classifier.getParameters());
-	}
-
-	private static void printScores(String label, double[] scores) {
-		System.out.printf("%s scores = [", label);
-		for (double score : scores)
-			System.out.printf(" %f", score);
-		System.out.println(" ]");
-	}
-
-	private static TrainingControl getControl(final boolean useAcceleration) {
-		return new TrainingControl() {
-			@Override
-			public double scoreTolerance() {
-				return 1e-14;
-			}
-			
-			@Override
-			public int maxIterations() {
-				return 0;
-			}
-			
-			@Override
-			public double gradientTolerance() {
-				return 0;
-			}
-
-			@Override
-			public double relativeScoreTolerance() {
-				return 0;
-			}
-
-			@Override
-			public boolean useAcceleration() {
-				return useAcceleration;
-			}
-		};
+	private static LoggedClassifierTrainer getTrainer(GoldData trainingData,
+			GoldData testingData) {
+		LoggedClassifierTrainer trainer = new LoggedClassifierTrainer(
+				ClassifierFactory.newTrainableClassifier(trainingData.numClasses(), trainingData.numFeatures()), 
+				trainingData, testingData);
+		return trainer;
 	}
 
 	private static GoldData getMatchData(final Collection<Match> matches) {
