6a1adebe4e2fade9595c39bc03e6f680a0312523
uoa1637653 <geoff.jarrad@hotmail.com.au>
2014-10-02 13:47:40 +0930

    Separated out tests and test data.

--- gaj-data-analysis/test/gaj/markov/test/MarkovTest.java
+++ gaj-data-analysis/test/gaj/markov/test/MarkovTest.java
@@ -12,21 +12,34 @@ import org.junit.Test;
 
 public class MarkovTest {
 
+    /** C(s_t, s_{t-1}). */
+    private static final DataMatrix transCounts = MatrixFactory.newMatrix(
+	    new double[][]{
+		    { 1, 4}, // C(s_1=sigma_1,s_2)
+		    { 4, 1 } // C(s_1=sigma_2,s_2)
+	    });
     private static final double EPSILON = 1e-6;
+    /** P(s_t|s_{t-1}). */
+    private static final DataMatrix transProbs =
+	    MarkovOneStepAnalyser.computeTransitions(transCounts);
+    /** P(s_1|start). */
+    private static final DataVector startProbs = VectorFactory.newVector(0.9, 0.1);
+    /** P(end|s_T). */
+    private static final DataVector endProbs = VectorFactory.newVector(0.1, 0.5);
+    /** p(x_t|s_t). */
+    private static final DataMatrix obsProbs = MatrixFactory.newMatrix(
+	    new double[][]{
+		    { 0.8, 0.3 }, // p(x_1|s_1)
+		    { 0.4, 0.9 }  // p(x_2|s_2)
+	    });
 
     private boolean equals(double x, double y) {
 	return Math.abs(x - y) < EPSILON;
     }
 
     @Test
-    public void test() {
-	DataMatrix jointProbs = MatrixFactory.newMatrix( // P(s_{t-1},s_t)
-		new double[][]{
-			{ 0.1, 0.4}, // P(s_1=sigma_1,s_2)
-			{ 0.4, 0.1 } // P(s_1=sigma_2,s_2)
-		}
-		);
-	DataMatrix transProbs = MarkovOneStepAnalyser.computeTransitions(jointProbs); // P(s_t|s_{t-1})
+    public void testTransitionProbs() {
+	System.out.println("testTransitionProbs:");
 	DataMatrix expectedTransProbs = MatrixFactory.newMatrix(
 		new double[][]{
 			{ 0.2, 0.8}, // P(s_2|s_1=sigma_1)
@@ -39,14 +52,11 @@ public class MarkovTest {
 	assertTrue(equals(transProbs.get(0, 1), 0.8));
 	assertTrue(equals(transProbs.get(1, 0), 0.8));
 	assertTrue(equals(transProbs.get(1, 1), 0.2));
+    }
 
-	DataVector startProbs = VectorFactory.newVector(0.9, 0.1); // P(s_1|start)
-	DataMatrix obsProbs = MatrixFactory.newMatrix( // p(x_t|s_t)
-		new double[][]{
-			{ 0.8, 0.3 }, // p(x_1|s_1)
-			{ 0.4, 0.9 }  // p(x_2|s_2)
-		}
-		);
+    @Test
+    public void testForwardAlgo() {
+	System.out.println("testForwardAlgo:");
 	DataMatrix alpha = MarkovOneStepLibrary.forwardProbabilities(obsProbs, startProbs, transProbs);
 	MatrixFactory.display("p(x_1,x_2,...,x_t,s_t|start)=", alpha, "\n");
 	/*
@@ -63,8 +73,11 @@ public class MarkovTest {
 		}
 		);
 	assertTrue(MatrixFactory.equals(expectedAlpha, alpha, EPSILON));
+    }
 
-	DataVector endProbs = VectorFactory.newVector(0.1, 0.5);   // P(end|s_N)
+    @Test
+    public void testBackwardAlgo() {
+	System.out.println("testBackwardAlgo:");
 	DataMatrix beta = MarkovOneStepLibrary.backwardProbabilities(obsProbs, endProbs, transProbs);
 	MatrixFactory.display("p(x_{t+1},x_{t+2},...,end|s_t,start)=", beta, "\n");
 	/*
@@ -80,7 +93,11 @@ public class MarkovTest {
 		}
 		);
 	assertTrue(MatrixFactory.equals(expectedBeta, beta, EPSILON));
+    }
 
+    @Test
+    public void testForwardBackwardAlgo() {
+	System.out.println("testForwardBackwardAlgo:");
 	DataMatrix gamma = MarkovOneStepLibrary.jointProbabilities(obsProbs, startProbs, endProbs, transProbs);
 	MatrixFactory.display("p(x_1,...,x_N,end,s_t|start)=", gamma, "\n");
 	/*
@@ -97,7 +114,11 @@ public class MarkovTest {
 	assertTrue(MatrixFactory.equals(expectedGamma, gamma, EPSILON));
 	double norm = gamma.getRow(0).sum();
 	assertTrue(Math.abs(norm - gamma.getRow(1).sum()) <= EPSILON);
+    }
 
+    @Test
+    public void testNormedForwardBackwardAlgo() {
+	System.out.println("testNormedForwardBackwardAlgo:");
 	DataMatrix pred = MarkovOneStepLibrary.posteriorProbabilities(obsProbs, startProbs, endProbs, transProbs);
 	MatrixFactory.display("p(s_t|start,x_1,...,x_N,end)=", pred, "\n");
 	/*
@@ -116,22 +137,29 @@ public class MarkovTest {
 	assertTrue(MatrixFactory.equals(expectedPred, pred, EPSILON));
 	assertTrue(Math.abs(pred.getRow(0).sum() - 1) <= EPSILON);
 	assertTrue(Math.abs(pred.getRow(1).sum() - 1) <= EPSILON);
-	assertTrue(Math.abs(pred.get(0, 0)*norm - gamma.get(0, 0)) <= EPSILON);
+    }
 
+    @Test
+    public void testAnalyser() {
+	System.out.println("testAnalyser:");
 	/*
 	 * Fudge the probabilities to produce the various state transition counts.
 	 */
 	DataVector initCounts = VectorFactory.scale(startProbs, 10);
-	VectorFactory.display("C(s_1|start)", initCounts, "\n");
-	DataMatrix transCounts = MatrixFactory.scale(jointProbs, 10);
-	MatrixFactory.display("C(s_t|s_{t-1})=", transCounts, "\n");
+	VectorFactory.display("C(s_1|start)=", initCounts, "\n");
 	DataVector finalCounts = VectorFactory.newVector(5.0/9, 5);
-	VectorFactory.display("C(end|s_N)", finalCounts, "\n");
+	VectorFactory.display("C(end|s_N)=", finalCounts, "\n");
 	MarkovOneStepAnalyser analyser = MarkovOneStepAnalyser.newAnalyser(
 		initCounts, finalCounts, transCounts);
-	DataMatrix pred2 = analyser.posteriorProbabilities(obsProbs, SequenceType.Complete);
-	MatrixFactory.display("p(s_t|start,x_1,...,x_N,end)=", pred2, "\n");
-	assertTrue(MatrixFactory.equals(expectedPred, pred2, EPSILON));
+	DataMatrix pred = analyser.posteriorProbabilities(obsProbs, SequenceType.Complete);
+	MatrixFactory.display("p(s_t|start,x_1,...,x_N,end)=", pred, "\n");
+
+	DataMatrix expectedPred = MatrixFactory.newMatrix(
+		new double[][] {
+			{ 0.986375, 0.013625 },
+			{ 0.025017, 0.974983 }
+		});
+	assertTrue(MatrixFactory.equals(expectedPred, pred, EPSILON));
     }
 
 }
