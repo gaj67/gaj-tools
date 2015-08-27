eb6a4455c609f7e8bdbb4fa29e8bba058ddd425b
uoa1637653 <geoff.jarrad@hotmail.com.au>
2015-08-27 16:21:21 +0930

    More corrections and junk removal.

--- gaj-text-analyser/test/gaj/text/test/loaders/CheckGutenbergProperties.java
+++ gaj-text-analyser/test/gaj/text/test/loaders/CheckGutenbergProperties.java
@@ -11,86 +11,96 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class CheckGutenbergProperties {
 
-	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.out.println("Arguments: <path-to-dictionary>");
-		} else {
-			read(args[0]);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Arguments: <path-to-dictionary>");
+        } else {
+            read(args[0]);
+        }
+    }
 
-	private static void read(String path) throws IOException {
-		try (BufferedReader in = new BufferedReader(new FileReader(path))) {
-			Map<String,AtomicInteger> tagTypes = new HashMap<>();
-			List<AtomicInteger> tagCounts = new ArrayList<>();
-			String line = null;
-			int wordCounter = 0;
-			int tagCounter = 0;
-			wordloop: while (true) {
-				if (line == null) {
-					line = in.readLine();
-				}
-				if (line == null) {
-					break;
-				}
-				if (!line.contains("<h1>")) {
-					line = null;
-					continue;
-				}
-				wordCounter++;
-				if (wordCounter % 1_000 == 0) {
-					System.out.printf("Processing word # %d...%n", wordCounter);
-					System.out.flush();
-				}
-				List<String> tags = new ArrayList<>();
-				while (true) {
-					line = in.readLine();
-					if (line == null || line.contains("<h1>")) {
-						countTagTypes(tagTypes, tags);
-						countTagFreqs(tagCounts, tags.size());
-						tagCounter += tags.size();
-						continue wordloop;
-					}
-					if (line.contains("<hw>")) {
-						tags.addAll(getTags(line));
-					}
-				}
-			}
-			System.out.printf("#words=%d, #tags=%d%n", wordCounter, tagCounter);
-		}
+    private static void read(String path) throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            Map<String,AtomicInteger> tagTypes = new HashMap<>();
+            List<AtomicInteger> tagCounts = new ArrayList<>();
+            String line = null;
+            int wordCounter = 0;
+            int tagCounter = 0;
+            wordloop: while (true) {
+                if (line == null) {
+                    line = in.readLine();
+                }
+                if (line == null) {
+                    break;
+                }
+                if (!line.contains("<h1>")) {
+                    line = null;
+                    continue;
+                }
+                wordCounter++;
+                if (wordCounter % 1_000 == 0) {
+                    System.out.printf("Processing word # %d...%n", wordCounter);
+                    System.out.flush();
+                }
+                List<String> tags = new ArrayList<>();
+                while (true) {
+                    line = in.readLine();
+                    if (line == null || line.contains("<h1>")) {
+                        countTagTypes(tagTypes, tags);
+                        countTagFreqs(tagCounts, tags.size());
+                        tagCounter += tags.size();
+                        continue wordloop;
+                    }
+                    if (line.contains("<hw>")) {
+                        tags.addAll(getTags(line));
+                    }
+                }
+            }
+            System.out.printf("#words=%d, #tags=%d%n", wordCounter, tagCounter);
+            System.out.printf("#tag-types=%d%ntag-types=%s%n", tagTypes.size(), tagTypes);
+            System.out.printf("tag-freqs=%s%n", tagCounts);
+        }
 
-	}
+    }
 
-	private static List<String> getTags(String line) {
-		List<String> tags = new ArrayList<>();    	
-		int sidx = -1;
-		while (true) {
-			sidx = (sidx < 0) ? line.indexOf("<tt>") : line.indexOf("<tt>", sidx);
-			if (sidx < 0) {
-				break;
-			}
-			sidx += 4;
-			int eidx = line.indexOf("</tt>", sidx);
-			if (eidx < 0) throw new IllegalStateException("Missing tag end on line: " + line);
-			tags.add(line.substring(sidx, eidx));
-		}
-		return tags;
-	}
+    private static List<String> getTags(String line) {
+        List<String> tags = new ArrayList<>();
+        int sidx = -1;
+        while (true) {
+            sidx = (sidx < 0) ? line.indexOf("<tt>") : line.indexOf("<tt>", sidx);
+            if (sidx < 0) {
+                break;
+            }
+            sidx += 4;
+            int eidx = line.indexOf("</tt>", sidx);
+            if (eidx < 0) {
+                throw new IllegalStateException("Missing tag end on line: " + line);
+            }
+            String tag = line.substring(sidx, eidx);
+            if (tag.contains("<") || tag.contains(">")) {
+                throw new IllegalStateException("Embedded tag within tag: " + tag + " on line: " + line);
+            }
+            tags.add(tag);
+        }
+        return tags;
+    }
 
-	private static void countTagTypes(Map<String,AtomicInteger> tagTypes, List<String> tags) {
-		for (String tag : tags) {
-			AtomicInteger count = tagTypes.get(tag);
-			if (count == null) {
-				tagTypes.put(tag, new AtomicInteger(1));
-			} else {
-				count.incrementAndGet();
-			}
-		}
-	}
+    private static void countTagTypes(Map<String,AtomicInteger> tagTypes, List<String> tags) {
+        for (String tag : tags) {
+            AtomicInteger count = tagTypes.get(tag);
+            if (count == null) {
+                tagTypes.put(tag, new AtomicInteger(1));
+            } else {
+                count.incrementAndGet();
+            }
+        }
+    }
 
-	private static void countTagFreqs(List<AtomicInteger> tagFreqs, int tagCount) {
-		while (tagFreqs.size() <= tagCount) tagFreqs.add(new AtomicInteger());
-		tagFreqs.get(tagCount).incrementAndGet();
-	}
-	
+    private static void countTagFreqs(List<AtomicInteger> tagFreqs, int tagCount) {
+        while (tagFreqs.size() <= tagCount) {
+            tagFreqs.add(new AtomicInteger());
+        }
+        tagFreqs.get(tagCount).incrementAndGet();
+    }
+
 }
