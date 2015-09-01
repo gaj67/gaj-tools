eafb0997b4f46e13482145588d9e020a18365c63
uoa1637653 <geoff.jarrad@hotmail.com.au>
2015-09-01 17:47:01 +0930

    Started use of TagSoup.

---
+++ gaj-text-analyser/test/gaj/text/freedictionary/ParseFiles.java
@@ -0,0 +1,37 @@
+package gaj.text.freedictionary;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.ccil.cowan.tagsoup.jaxp.SAXParserImpl;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ParseFiles {
+
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.out.println("Arguments: <path-to-definition(s)>+");
+        } else {
+            for (String arg : args) {
+                File path = new File(arg);
+                if (path.isFile()) {
+                    analyseFile(path);
+                } //TODO analyseDirectory(path);
+            }
+        }
+    }
+
+    private static void analyseFile(File path) {
+        try (InputStream is = new FileInputStream(path)) {
+            SAXParserImpl parser = SAXParserImpl.newInstance(null);
+            DefaultHandler handler = new FreeDictionaryHandler();
+            parser.parse(is, handler);
+        } catch (IOException | SAXException e) {
+            System.out.printf("Failed to analyse: " + path + " because: " + e.getMessage());
+        }
+
+    }
+
+}
---
+++ gaj-text-analyser/test/gaj/text/freedictionary/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Tools to analyse pages from the FreeDictionary web site.
+ */
+package gaj.text.freedictionary;
